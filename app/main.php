aef5d3156d65e4f1748873729a5372018dd8b998
lyucean <lyucean@gmail.com>
2023-08-29 23:17:38 +0300

    Подключим простую либу для работы с БД

--- app/main.php
+++ app/main.php
@@ -2,4 +2,18 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-echo $_ENV['MYSQL_HOST'];
+
+
+$db = new MysqliDb(
+  array(
+    'host' => $_ENV['MYSQL_HOST'],
+    'username' => $_ENV['MYSQL_USER'],
+    'password' => $_ENV['MYSQL_PASSWORD'],
+    'db' => $_ENV['MYSQL_DATABASE'],
+    'port' => $_ENV['MYSQL_PORT'],
+    'prefix' => '',
+    'charset' => 'utf8'
+  )
+);
+
+
