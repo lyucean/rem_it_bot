bfe12540050f705db4edd21a89d42fd6defb6a24
lyucean <lyucean@gmail.com>
2023-08-29 22:47:03 +0300

    Подключим конфик

---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
+$dotenv->load();
+
+$dotenv->required('ENVIRONMENT')->notEmpty();
+$dotenv->required('MYSQL_HOST')->notEmpty();
+$dotenv->required('MYSQL_USER')->notEmpty();
+$dotenv->required('MYSQL_PASSWORD')->notEmpty();
+$dotenv->required('MYSQL_DATABASE')->notEmpty();
+$dotenv->required('MYSQL_PORT')->notEmpty();
