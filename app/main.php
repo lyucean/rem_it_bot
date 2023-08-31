c54f0c6c983df25b06a5d15961fc02b5de17dbc0
lyucean <lyucean@gmail.com>
2023-09-01 02:16:57 +0300

    Добавил отправку сообщений по рассписанию

--- app/main.php
+++ app/main.php
@@ -3,7 +3,13 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use RIB\model\Processing;
+use RIB\model\Schedule;
 
-// Reply to all messages, once per second
-$processing = new Processing();
-$processing->check();
+// Проверяем расписание, нужно ли кому-то отправить сообщение
+(new  Schedule())->check();
+
+// Ответ на все сообщения раз в секунду
+(new Processing())->check();
+
+// Давайте создадим список рассылки на день.
+(new  Schedule())->generate();
---
+++ app/model/Schedule.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace RIB\model;
+
+use DateTime;
+use Exception;
+use RIB\core\Model;
+
+class Schedule extends Model
+{
+
+    /**
+     * Checking to see if it's time for an alert, if it is, it sends it out
+     */
+    public function check(): void
+    {
+        foreach ($this->db->getSendingDailyNow() as $item) {
+
+            $message = $this->db->getMessagePrepared($item['chat_id']);
+
+            // confirm
+            $this->db->setScheduleDailyStatusSent($item['schedule_daily_id']);
+
+            if (empty($message)) {
+                continue;
+            }
+
+            $answer = $message['text'].' /_'.$message['message_id'];
+
+
+            // if this image
+            if (!empty($message['image'])) {
+                $img = curl_file_create($_ENV['DIR_FILE'].$message['image'], 'image/jpeg');
+
+
+                $answer = $this->telegram->sendPhoto(
+                  [
+                    'chat_id' => $item['chat_id'],
+                    'photo' => $img,
+                    'caption' => fix_breaks($answer)
+                  ]
+                );
+
+                // if there is an error
+//                if (!$answer['ok']) {
+//                    \Sentry\captureMessage(
+//                      'chat_id: '.$item['chat_id']
+//                      .'description: '.$answer['description']
+//                      .'error_code: '.$answer['error_code']
+//                    );
+//                }
+                continue;
+            }
+
+
+            // default is text
+            $this->telegram->sendMessage(
+              [
+                'chat_id' => $item['chat_id'],
+                'text' => fix_breaks($answer),
+              ]
+            );
+
+        }
+    }
+
+    /**
+     * Generates the date and time of the alert in mysql format with an offset from the time zone
+     * @param  int  $hour_start
+     * @param  int  $hour_end
+     * @param  int  $time_zone_offset
+     * @return string
+     * @throws Exception
+     */
+    public function createDateTimeForSchedule(int $hour_start, int $hour_end, int $time_zone_offset): string
+    {
+        $date_starting = gmdate('Y-m-d '.rand($hour_start, $hour_end).':'.rand(10, 59).':s');
+        $date = new DateTime($date_starting);
+        $date->modify('+'.(-1) * $time_zone_offset.' hours');
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Fills out the schedule of notifications for the current day
+     */
+    public function generate(): void
+    {
+        // every day we will create a schedule for today
+        if ('0' != gmdate("G") || '01' != gmdate("i")) {
+            return;
+        }
+
+        foreach ($this->db->getSchedules() as $item) {
+            // how many notifications to send per day
+            for ($i = 0; $i < $item['quantity']; $i++) {
+                $this->db->addSendingDailyNow(
+                  [
+                    'chat_id' => $item['chat_id'],
+                    'date_time' => $this->createDateTimeForSchedule(
+                      $item['hour_start'],
+                      $item['hour_end'],
+                      $item['time_zone_offset']
+                    ),
+                    'status_sent' => 0,
+                  ]
+                );
+            }
+        }
+    }
+}
