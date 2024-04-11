d25898c2b45dc90431d1325afc167ed074c8494d
lyucean <lyucean@gmail.com>
2024-04-11 18:19:18 +0300

    Перезапустим

--- app/lib/logs.php
+++ app/lib/logs.php
@@ -8,15 +8,9 @@ class Logs
 
     private function __construct()
     {
-        // Private constructor with logging setup
-        if (!empty($_ENV['BETTERSTACK_TOKEN'])) {
-            $logger = new Logger($_ENV['TELEGRAM_BOT_NAME'] . $_ENV['ENVIRONMENT']); // Initialize Logger
-            $logger->pushHandler(new LogtailHandler($_ENV['BETTERSTACK_TOKEN']));
-            self::$instance = $logger; // Store the Logger instance
-        }else{
-            self::$instance = new Logger('no_token_logger'); // Create a default logger
-            self::$instance->critical('BETTERSTACK_TOKEN отсутствует');
-        }
+        $logger = new Logger($_ENV['TELEGRAM_BOT_NAME'] . $_ENV['ENVIRONMENT']); // Initialize Logger
+        $logger->pushHandler(new LogtailHandler($_ENV['BETTERSTACK_TOKEN']));
+        self::$instance = $logger; // Store the Logger instance
     }
 
     public static function getInstance(): Logger
