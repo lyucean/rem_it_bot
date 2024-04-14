89817ed9b032a7b5f512885d71f1dffc39b7db85
lyucean <lyucean@gmail.com>
2024-04-15 00:17:55 +0300

    Снизим нагрузку на сервере

--- app/main.php
+++ app/main.php
@@ -20,21 +20,27 @@ $cycleCount = 0; // счетчик цикла
 
 // Бесконечный цикл, который будет вызывать основной файл скрипта
 while (true) {
+    // Ответ на все сообщения раз в секунду
+    (new Processing())->check();
 
     // пускай выполняется каждый 60 цикл
     if ($cycleCount % 20 === 0) {
         heartbeat();
+        if (1000 < $cycleCount) {
+            $cycleCount = 0;
+        }
     }
     $cycleCount++;
 
     // Проверяем расписание, нужно ли кому-то отправить сообщение
-    (new  Schedule())->check();
-
-    // Ответ на все сообщения раз в секунду
-    (new Processing())->check();
+    if ($cycleCount % 60 === 0) { // пускай выполняется каждый 60 цикл
+        (new  Schedule())->check();
+    }
 
     // Давайте создадим список рассылки на день.
-    (new  Schedule())->generate();
+    if ($cycleCount % 600 === 0) { // пускай выполняется каждый 101 цикл
+        (new  Schedule())->generate();
+    }
 
     sleep($periodChecked);
 }
